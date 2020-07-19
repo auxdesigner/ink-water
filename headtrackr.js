a2da0028826a19eb47b7d6023c2fea326931ce1b
Liu Liu <liu@benchling.com>
2020-07-18 19:13:21 -0700

    camera message

--- headtrackr.js
+++ headtrackr.js
@@ -1600,32 +1600,32 @@
             var timeout;
 
             // create element and attach to body
-            var d = document.createElement('div'),
-                d2 = document.createElement('div'),
-                p = document.createElement('p');
-            d.setAttribute('id', 'headtrackerMessageDiv');
+            // var d = document.createElement('div'),
+            //     d2 = document.createElement('div'),
+            //     p = document.createElement('p');
+            // d.setAttribute('id', 'headtrackerMessageDiv');
 
-            d.style.left = "20%";
-            d.style.right = "20%";
-            d.style.top = "30%";
-            d.style.fontSize = "90px";
-            d.style.color = "#777";
-            d.style.position = "absolute";
-            d.style.fontFamily = "Helvetica, Arial, sans-serif";
-            d.style.zIndex = '100002';
+            // d.style.left = "20%";
+            // d.style.right = "20%";
+            // d.style.top = "30%";
+            // d.style.fontSize = "90px";
+            // d.style.color = "#777";
+            // d.style.position = "absolute";
+            // d.style.fontFamily = "Helvetica, Arial, sans-serif";
+            // d.style.zIndex = '100002';
 
-            d2.style.marginLeft = "auto";
-            d2.style.marginRight = "auto";
-            d2.style.width = "100%";
-            d2.style.textAlign = "center";
-            d2.style.color = "#fff";
-            d2.style.backgroundColor = "#444";
-            d2.style.opacity = "0.5";
+            // d2.style.marginLeft = "auto";
+            // d2.style.marginRight = "auto";
+            // d2.style.width = "100%";
+            // d2.style.textAlign = "center";
+            // d2.style.color = "#fff";
+            // d2.style.backgroundColor = "#444";
+            // d2.style.opacity = "0.5";
 
-            p.setAttribute('id', 'headtrackerMessage');
-            d2.appendChild(p);
-            d.appendChild(d2);
-            document.body.appendChild(d);
+            // p.setAttribute('id', 'headtrackerMessage');
+            // d2.appendChild(p);
+            // d.appendChild(d2);
+            // document.body.appendChild(d);
 
             var supportMessages = {
                 "no getUserMedia": "getUserMedia is not supported in your browser :(",
@@ -1658,8 +1658,10 @@
                     var messagep = document.getElementById('headtrackerMessage');
                     messagep.innerHTML = supportMessages[event.status];
                     window.setTimeout(function() { messagep.innerHTML = 'added fallback video for demo'; }, 2000);
-                    window.setTimeout(function() { messagep.innerHTML = '';
-                        override = false; }, 4000);
+                    window.setTimeout(function() {
+                        messagep.innerHTML = '';
+                        override = false;
+                    }, 4000);
                 }
             }, true);
 
