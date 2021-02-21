4f38164a9925179ca780a9d1ff8eecfd667353d8
Liu Liu <liu@benchling.com>
2021-02-20 20:30:59 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+if (location.protocol !== 'https:') {
+    location.replace(`https:${location.href.substring(location.protocol.length)}`);
+}
+
 // var
 var $body = $('body'),
     $tile = $('#tile'),
